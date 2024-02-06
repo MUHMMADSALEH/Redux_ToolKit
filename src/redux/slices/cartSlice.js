e53483e17bf5584fbc6e2dd296872d9b14a1f9f7
MUHMMAD SALEH <khanmohdsaleh2002@gmail.com>
2024-02-07 00:03:26 +0530

    first commit

---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {createSlice, createSelector} from "@reduxjs/toolkit"
+
+const   cartSlice   =   createSlice({
+    name:"Cart",
+    initialState:[],
+    reducers:{
+        addItem: (state,action)=>{
+            state.push(action.payload);
+        }
+    }
+})
+export const getItemSelector=createSelector(state=>state.cart,state=>state);
+export const {addItem} =cartSlice.actions
+export default cartSlice.reducer;
